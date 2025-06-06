9f046ab1248df6bf795047bd3c7f793c730245ca
milkfish <i@mxd.moe>
2025-06-06 19:52:20 +0800

    feat: 添加容器优雅退出支持

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 定义信号处理函数
+handle_sigterm() {
+    echo "收到 SIGTERM 信号，正在优雅关闭..."
+    # 向 Mirai Console 发送 stop 命令
+    echo "stop" > /proc/1/fd/0
+    # 等待进程退出
+    wait $mirai_pid
+}
+
+# 注册 SIGTERM 信号处理
+trap 'handle_sigterm' SIGTERM
+
 # 生成配置文件
 dockerize -template /app/overflow/overflow.json.tmpl:/app/overflow/overflow.json \
          -template /app/overflow/config/net.mamoe.mirai-api-http/setting.yml.tmpl:/app/overflow/config/net.mamoe.mirai-api-http/setting.yml
@@ -13,4 +25,8 @@ cat /app/overflow/config/net.mamoe.mirai-api-http/setting.yml
 
 # 启动程序
 cd /app/overflow
-java -cp "./content/*" net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+java -cp "./content/*" net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader &
+mirai_pid=$!
+
+# 等待程序退出
+wait $mirai_pid
